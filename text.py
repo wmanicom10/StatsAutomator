7d1f8915763204051ae84273141caa0e1f8fe330
Will Manicom <145696695+wmanicom10@users.noreply.github.com>
2024-09-20 09:51:57 -0400

    Add files via upload

--- text.py
+++ text.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def print_header():
-    print("Official list of the Top 100 All-Time Worldwide Box Office films, based on The Numbers\n")
+    print("List of the Top 100 All-Time Worldwide Box Office films, based on The Numbers\n")
 
     formatted_date = datetime.now().strftime("%m/%d/%y")
     print("Last Update - " + formatted_date + "\n")
