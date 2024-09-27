4cfc0b392742997dd524c7889e01ab88f0bba89a
Will Manicom <145696695+wmanicom10@users.noreply.github.com>
2024-09-27 09:15:50 -0400

    Add files via upload

--- text.py
+++ text.py
@@ -16,7 +16,7 @@ def print_rating():
     print("Highest rated film in the list")
     print("The Lord of the Rings: The Return of the King (4.51)\n")
     print("Lowest rated film in the list")
-    print("Transformers: Age of Extinction (2.04)\n")
+    print("Transformers: Age of Extinction (2.05)\n")
 
 
 def print_billion():
